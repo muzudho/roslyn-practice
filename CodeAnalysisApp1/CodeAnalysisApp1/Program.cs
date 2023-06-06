c4449c853eb3b99f6bcf4e47fabed636389b55af
むずでょ <muzudho1@gmail.com>
2023-06-07 05:36:33 +0900

    Update Program.cs

--- CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
+++ CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,11 +17,18 @@ namespace CodeAnalysisApp1
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static string ReadTextFile()
         {
-            // 読込対象のテキスト
-            const string programText =
-@"using System;
+            // ファイルパス
+            var filePath = "C:\\Users\\むずでょ\\Documents\\Unity Projects\\RMU-1-00-00-Research-Project\\Assets\\RPGMaker\\Codebase\\CoreSystem\\Knowledge\\JsonStructure\\ChapterJson.cs";
+
+            // テキスト・ファイル読込
+            return File.ReadAllText(filePath);
+        }
+
+        static string GetExample()
+        {
+            return @"using System;
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -35,23 +43,51 @@ namespace HelloWorld
         }
     }
 }";
+        }
 
+        static async Task Main(string[] args)
+        {
+            // 読込対象のテキスト
+            string programText = ReadTextFile();
+            // string programText = GetExample();
+
+            //
             // テキストをパースして、ツリー作成
+            //
             SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
 
+            //
             // ツリーから根を取得
+            //
             CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
 
+            //
             // 根のデータ
+            //
             Console.WriteLine($"The tree is a {root.Kind()} node.");
+            // The tree is a CompilationUnit node.
+
+            //
+            // 根の要素数
+            //
             Console.WriteLine($"The tree has {root.Members.Count} elements in it.");
+            // The tree has 1 elements in it.
+
+            //
+            // Using 句の数
+            //
             Console.WriteLine($"The tree has {root.Usings.Count} using statements. They are:");
+            // The tree has 4 using statements.They are:
 
             // 根にぶら下がっている Using 句を出力
             foreach (UsingDirectiveSyntax element in root.Usings)
             {
                 Console.WriteLine($"\t{element.Name}");
             }
+            // System
+            // System.Collections
+            // System.Linq
+            // System.Text
 
             // 根にぶら下がっている最初のものの種類
             MemberDeclarationSyntax firstMember = root.Members[0];
