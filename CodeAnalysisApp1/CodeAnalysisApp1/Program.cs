a3acac6b27d9860491635fa74fa931d22ac2a195
むずでょ <muzudho1@gmail.com>
2023-06-07 05:24:01 +0900

    Update Program.cs

--- CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
+++ CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
@@ -53,76 +53,60 @@ namespace HelloWorld
                 Console.WriteLine($"\t{element.Name}");
             }
 
-            /*
-            // Attempt to set the version of MSBuild.
-            var visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
-            var instance = visualStudioInstances.Length == 1
-                // If there is only one instance of MSBuild on this machine, set that as the one to use.
-                ? visualStudioInstances[0]
-                // Handle selecting the version of MSBuild you want to use.
-                : SelectVisualStudioInstance(visualStudioInstances);
+            // 根にぶら下がっている最初のものの種類
+            MemberDeclarationSyntax firstMember = root.Members[0];
+            Console.WriteLine($"The first member is a {firstMember.Kind()}.");
+            // The first member is a NamespaceDeclaration.
 
-            Console.WriteLine($"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
 
-            // NOTE: Be sure to register an instance with the MSBuildLocator 
-            //       before calling MSBuildWorkspace.Create()
-            //       otherwise, MSBuildWorkspace won't MEF compose.
-            MSBuildLocator.RegisterInstance(instance);
+            //
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+            Console.WriteLine($"There are {programDeclaration.Members.Count} members declared in the {programDeclaration.Identifier} class.");
+            // There are 1 members declared in the Program class.
 
-            using (var workspace = MSBuildWorkspace.Create())
+            Console.WriteLine($"The first member is a {programDeclaration.Members[0].Kind()}.");
+            // The first member is a MethodDeclaration.
+
+            //
+            // プログラム中の宣言メンバーの１つ目
+            //
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+
+            //
+            // 返却値の型
+            //
+            Console.WriteLine($"The return type of the {mainDeclaration.Identifier} method is {mainDeclaration.ReturnType}.");
+            // The return type of the Main method is void.
+
+            //
+            // 引数の個数
+            //
+            Console.WriteLine($"The method has {mainDeclaration.ParameterList.Parameters.Count} parameters.");
+            // The method has 1 parameters.
+
+            //
+            // 各引数
+            //
+            foreach (ParameterSyntax item in mainDeclaration.ParameterList.Parameters)
             {
-                // Print message for WorkspaceFailed event to help diagnosing project load failures.
-                workspace.WorkspaceFailed += (o, e) => Console.WriteLine(e.Diagnostic.Message);
-
-                var solutionPath = args[0];
-                Console.WriteLine($"Loading solution '{solutionPath}'");
-
-                // Attach progress reporter so we print projects as they are loaded.
-                var solution = await workspace.OpenSolutionAsync(solutionPath, new ConsoleProgressReporter());
-                Console.WriteLine($"Finished loading solution '{solutionPath}'");
-
-                // TODO: Do analysis on the projects in the loaded solution
+                //
+                // 引数１つ分
+                //
+                Console.WriteLine($"The type of the {item.Identifier} parameter is {item.Type}.");
+                // The type of the args parameter is string[].
             }
-            */
+
+            //
+            // 関数定義文
+            //
+            Console.WriteLine($"The body text of the {mainDeclaration.Identifier} method follows:");
+            Console.WriteLine(mainDeclaration.Body?.ToFullString());
+            // {
+            //     Console.WriteLine("Hello, World!");
+            // }
+
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
         }
-        /*
-        private static VisualStudioInstance SelectVisualStudioInstance(VisualStudioInstance[] visualStudioInstances)
-        {
-            Console.WriteLine("Multiple installs of MSBuild detected please select one:");
-            for (int i = 0; i < visualStudioInstances.Length; i++)
-            {
-                Console.WriteLine($"Instance {i + 1}");
-                Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
-                Console.WriteLine($"    Version: {visualStudioInstances[i].Version}");
-                Console.WriteLine($"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
-            }
-
-            while (true)
-            {
-                var userResponse = Console.ReadLine();
-                if (int.TryParse(userResponse, out int instanceNumber) &&
-                    instanceNumber > 0 &&
-                    instanceNumber <= visualStudioInstances.Length)
-                {
-                    return visualStudioInstances[instanceNumber - 1];
-                }
-                Console.WriteLine("Input not accepted, try again.");
-            }
-        }
-
-        private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
-        {
-            public void Report(ProjectLoadProgress loadProgress)
-            {
-                var projectDisplay = Path.GetFileName(loadProgress.FilePath);
-                if (loadProgress.TargetFramework != null)
-                {
-                    projectDisplay += $" ({loadProgress.TargetFramework})";
-                }
-
-                Console.WriteLine($"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
-            }
-        }
-        */
     }
 }
