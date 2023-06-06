8a988c97add53c7268df3a4769e7f7e5e23e512f
むずでょ <muzudho1@gmail.com>
2023-06-07 06:05:31 +0900

    Update Program.cs

--- CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
+++ CodeAnalysisApp1/CodeAnalysisApp1/Program.cs
@@ -93,49 +93,168 @@ namespace HelloWorld
             // そのメソッド数
             Console.WriteLine($"There are {programDeclaration.Members.Count} members declared in the {programDeclaration.Identifier} class.");
             // There are 1 members declared in the Program class.
+            // There are 13 members declared in the ChapterJson class.
 
             Console.WriteLine($"The first member is a {programDeclaration.Members[0].Kind()}.");
             // The first member is a MethodDeclaration.
+            // The first member is a FieldDeclaration.
 
-            //
-            // プログラム中の宣言メンバーの１つ目
-            //
-            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
-
-            //
-            // 返却値の型
-            //
-            Console.WriteLine($"The return type of the {mainDeclaration.Identifier} method is {mainDeclaration.ReturnType}.");
-            // The return type of the Main method is void.
-
-            //
-            // 引数の個数
-            //
-            Console.WriteLine($"The method has {mainDeclaration.ParameterList.Parameters.Count} parameters.");
-            // The method has 1 parameters.
-
-            //
-            // 各引数
-            //
-            foreach (ParameterSyntax item in mainDeclaration.ParameterList.Parameters)
+            switch (programDeclaration.Members[0].Kind())
             {
-                //
-                // 引数１つ分
-                //
-                Console.WriteLine($"The type of the {item.Identifier} parameter is {item.Type}.");
-                // The type of the args parameter is string[].
+                // メソッドなら
+                case SyntaxKind.MethodDeclaration:
+                    {
+                        //
+                        // プログラム中の宣言メンバーの１つ目
+                        //
+                        var methodDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+
+                        //
+                        // 返却値の型
+                        //
+                        Console.WriteLine($"The return type of the {methodDeclaration.Identifier} method is {methodDeclaration.ReturnType}.");
+                        // The return type of the Main method is void.
+
+                        //
+                        // 引数の個数
+                        //
+                        Console.WriteLine($"The method has {methodDeclaration.ParameterList.Parameters.Count} parameters.");
+                        // The method has 1 parameters.
+
+                        //
+                        // 各引数
+                        //
+                        foreach (ParameterSyntax item in methodDeclaration.ParameterList.Parameters)
+                        {
+                            //
+                            // 引数１つ分
+                            //
+                            Console.WriteLine($"The type of the {item.Identifier} parameter is {item.Type}.");
+                            // The type of the args parameter is string[].
+                        }
+
+                        //
+                        // 関数定義文
+                        //
+                        Console.WriteLine($"The body text of the {methodDeclaration.Identifier} method follows:");
+                        Console.WriteLine(methodDeclaration.Body?.ToFullString());
+                        // {
+                        //     Console.WriteLine("Hello, World!");
+                        // }
+
+                        var argsParameter = methodDeclaration.ParameterList.Parameters[0];
+                    }
+                    break;
+
+                // フィールドの宣言部なら
+                case SyntaxKind.FieldDeclaration:
+                    {
+                        //
+                        // プログラム中の宣言メンバーの１つ目
+                        //
+                        var fieldDeclaration = (FieldDeclarationSyntax)programDeclaration.Members[0];
+
+                        Console.WriteLine($"fullString: {fieldDeclaration.ToFullString()}.");
+                        //            fullString:         /// <summary>
+                        //                                /// ?? 章Idの前に
+                        //                                /// </summary>
+                        //public int beforeChapterId;
+
+                        // コメント、アクセス修飾子、戻り値の型、名前はありそうだが
+
+                        Console.WriteLine($"AttributeLists: {fieldDeclaration.AttributeLists}");
+                        // AttributeLists:
+
+                        Console.WriteLine($"ContainsAnnotations: {fieldDeclaration.ContainsAnnotations}");
+                        // ContainsAnnotations: False
+
+                        Console.WriteLine($"ContainsDiagnostics: {fieldDeclaration.ContainsDiagnostics}");
+                        // ContainsDiagnostics: False
+
+                        Console.WriteLine($"ContainsDirectives:  {fieldDeclaration.ContainsDirectives}");
+                        // ContainsDirectives:  False
+
+                        Console.WriteLine($"ContainsSkippedText: {fieldDeclaration.ContainsSkippedText}");
+                        // ContainsSkippedText: False
+
+                        Console.WriteLine($"Declaration:         {fieldDeclaration.Declaration}");
+                        // Declaration:         int beforeChapterId
+
+                        Console.WriteLine($"FullSpan:            {fieldDeclaration.FullSpan}");
+                        // FullSpan:            [297..404)
+
+                        Console.WriteLine($"HasLeadingTrivia:    {fieldDeclaration.HasLeadingTrivia}");
+                        // HasLeadingTrivia:    True
+
+                        Console.WriteLine($"HasStructuredTrivia: {fieldDeclaration.HasStructuredTrivia}");
+                        // HasStructuredTrivia: True
+
+                        Console.WriteLine($"HasTrailingTrivia:   {fieldDeclaration.HasTrailingTrivia}");
+                        // HasTrailingTrivia:   True
+
+                        Console.WriteLine($"IsMissing:           {fieldDeclaration.IsMissing}");
+                        // IsMissing:           False
+
+                        Console.WriteLine($"IsStructuredTrivia:  {fieldDeclaration.IsStructuredTrivia}");
+                        // IsStructuredTrivia:  False
+
+                        Console.WriteLine($"Language:            {fieldDeclaration.Language}");
+                        // Language:            C#
+
+                        Console.WriteLine($"Modifiers:           {fieldDeclaration.Modifiers}");
+                        // Modifiers:           public
+
+                        Console.WriteLine($"Parent:              {fieldDeclaration.Parent}");
+                        // Parent:              [Serializable]
+                        //public class ChapterJson : IJsonStructure
+                        //    {
+                        //        /// <summary>
+                        //        /// ?? 章Idの前に
+                        //        /// </summary>
+                        //        public int beforeChapterId;
+                        // 以下略
+
+                        Console.WriteLine($"ParentTrivia:        {fieldDeclaration.ParentTrivia}");
+                        // ParentTrivia:
+
+                        Console.WriteLine($"RawKind:             {fieldDeclaration.RawKind}");
+                        // RawKind:             8873
+
+                        Console.WriteLine($"SemicolonToken:      {fieldDeclaration.SemicolonToken}");
+                        // SemicolonToken:      ;
+
+                        Console.WriteLine($"Span:                {fieldDeclaration.Span}");
+                        // Span:                [375..402)
+
+                        Console.WriteLine($"SpanStart:           {fieldDeclaration.SpanStart}");
+                        // SpanStart:           375
+
+                        Console.WriteLine($"SyntaxTree:          {fieldDeclaration.SyntaxTree}");
+                        // SyntaxTree:          using System;
+                        //                        using System.Collections.Generic;
+                        //
+                        ///// <summary>
+                        ///// ?? JSON構造体
+                        ///// </summary>
+                        //namespace RPGMaker.Codebase.CoreSystem.Knowledge.JsonStructure
+                        //    {
+                        //        /// <summary>
+                        //        /// ?? 章JSON
+                        //        /// </summary>
+                        //        [Serializable]
+                        //        public class ChapterJson : IJsonStructure
+                        //        {
+                        //            /// <summary>
+                        //            /// ?? 章Idの前に
+                        //            /// </summary>
+                        //            public int beforeChapterId;
+
+                        //
+                        // ドキュメント・コメントをうまく取る方法が無くないか？
+                        //
+                    }
+                    break;
             }
-
-            //
-            // 関数定義文
-            //
-            Console.WriteLine($"The body text of the {mainDeclaration.Identifier} method follows:");
-            Console.WriteLine(mainDeclaration.Body?.ToFullString());
-            // {
-            //     Console.WriteLine("Hello, World!");
-            // }
-
-            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
         }
 
         /// <summary>
